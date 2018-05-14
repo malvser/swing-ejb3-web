79cae5db1be69f53cd0a59a75adb31faf4c03450
serg <malovsergii@gmail.com>
2018-05-14 16:25:55 +0300

    config and filter

---
+++ ittest-web/src/main/java/com/it/web/config/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.it.web.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configuration of JAX-RS endpoints.
+ * @author andrey
+ *
+ */
+@ApplicationPath("api")
+public class JAXRSConfiguration extends Application{
+}
